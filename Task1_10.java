cde49a079dca3b2bc3c8223217f72137634544b6
Abdul-Khadar33 <vtu30308@veltech.edu.in>
2026-01-23 10:14:37 +0530

    Update Task1_10.java

--- Task1_10.java
+++ Task1_10.java
@@ -1,5 +1,7 @@
-public class Task1_10 {
-    public int lastDigitSum(int input1, int input2)
+import java.util.*;
+public class Task1_10
+{
+    public static int lastDigitSum(int input1, int input2)
     {
         if(input1 < 0)
         {
@@ -7,16 +9,20 @@ public class Task1_10 {
         }
         if(input2 < 0)
         {
-            input2 = - input2;
+            input2 = -input2;
         }
-        int lastDigit1 = input1 % 10;
-        int lastDigit2 = input2 % 10;
-        return lastDigit1 + lastDigit2;
+        int digit1 = input1 % 10;
+        int digit2 = input2 % 10;
+        return digit1 + digit2;
     }
-
     public static void main(String args[])
     {
-        Task10 t = new Task10();
-        System.out.println(t.lastDigitSum(123, 456));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int num2 = sc.nextInt();
+        int result = lastDigitSum(num1,num2);
+        System.out.println(result);
     }
 }
